8d30ee8f711a5624358b609baf2e447ea7356a75
aixueyuan <stuejiv88bhny23@gmail.com>
2025-10-28 12:31:51 +0800

    Update 2024_02_21_000000_add_segmented_title_to_discussions.php

--- migrations/2024_02_21_000000_add_segmented_title_to_discussions.php
+++ migrations/2024_02_21_000000_add_segmented_title_to_discussions.php
@@ -2,7 +2,29 @@
 
 use Flarum\Database\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 
-return Migration::addColumns('discussions', [
-    'segmented_title' => ['text', 'nullable' => true],
-]);
+return new class extends Migration
+{
+    public function up()
+    {
+        // 添加 'segmented_title' 字段
+        $this->schema()->table('discussions', function (Blueprint $table) {
+            $table->text('segmented_title')->nullable();
+        });
+
+        // 使用 DB 执行 SQL 语句添加 FULLTEXT 索引
+        DB::statement('ALTER TABLE discussions ADD FULLTEXT INDEX segmented_title_index (segmented_title);');
+    }
+
+    public function down()
+    {
+        // 删除 'segmented_title' 字段
+        $this->schema()->table('discussions', function (Blueprint $table) {
+            $table->dropColumn('segmented_title');
+        });
+
+        // 删除 FULLTEXT 索引
+        DB::statement('ALTER TABLE discussions DROP INDEX segmented_title_index;');
+    }
+};
