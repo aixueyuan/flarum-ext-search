a8ae7a9ca493ff542cbb4f5d5d59f119230d2761
aixueyuan <stuejiv88bhny23@gmail.com>
2025-10-28 12:32:20 +0800

    Update 2024_02_21_000000_add_segmented_title_to_discussions.php

--- migrations/2024_02_21_000000_add_segmented_title_to_discussions.php
+++ migrations/2024_02_21_000000_add_segmented_title_to_discussions.php
@@ -8,23 +8,19 @@ return new class extends Migration
 {
     public function up()
     {
-        // 添加 'segmented_title' 字段
         $this->schema()->table('discussions', function (Blueprint $table) {
             $table->text('segmented_title')->nullable();
         });
 
-        // 使用 DB 执行 SQL 语句添加 FULLTEXT 索引
         DB::statement('ALTER TABLE discussions ADD FULLTEXT INDEX segmented_title_index (segmented_title);');
     }
 
     public function down()
     {
-        // 删除 'segmented_title' 字段
         $this->schema()->table('discussions', function (Blueprint $table) {
             $table->dropColumn('segmented_title');
         });
 
-        // 删除 FULLTEXT 索引
         DB::statement('ALTER TABLE discussions DROP INDEX segmented_title_index;');
     }
 };
