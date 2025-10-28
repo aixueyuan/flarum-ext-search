8694c40ae8a1657b39fec3f41de5387ca410ecc5
aixueyuan <stuejiv88bhny23@gmail.com>
2025-10-28 14:55:36 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -2,14 +2,22 @@
 
 use Flarum\Discussion\Event\Saving;
 use Flarum\Discussion\Search\DiscussionSearcher;
+use Flarum\Discussion\Search\Fulltext\DriverInterface;
 use Flarum\Extend;
+use Illuminate\Contracts\Container\Container;
 use Aixueyuan\Search\Controller\ListSuggestWordsController;
+use Aixueyuan\Search\Driver\MySqlDiscussionTitleDriver;
 use Aixueyuan\Search\Gambit\TitleGambit;
 use Aixueyuan\Search\Listener\UpdateDiscussionTokens;
 
 return [
     (new Extend\SimpleFlarumSearch(DiscussionSearcher::class))
         ->setFullTextGambit(TitleGambit::class),
+
+    (new Extend\ServiceProvider())
+        ->register(function (Container $container) {
+            $container->bind(DriverInterface::class, MySqlDiscussionTitleDriver::class);
+        }),
 
     (new Extend\Event())
         ->listen(Saving::class, UpdateDiscussionTokens::class),
